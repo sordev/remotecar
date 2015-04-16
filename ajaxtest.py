0f1b75d2c0a0f44a551688708130d294cc95b88c
Ochir Sanjaadorj <osanjaadorj@bluefountainmedia.com>
2015-04-16 11:27:41 -0400

    web remote files

---
+++ ajaxtest.py
@@ -0,0 +1,11 @@
+﻿#! python
+
+import sys, json
+
+result = {'success':'true','message':'The Command Completed Successfully'};
+
+myjson = json.load(sys.stdin)
+# Do something with 'myjson' object
+
+print 'Content-Type: application/json\n\n'
+print json.dumps(result)    # or "json.dump(result, sys.stdout)"
--- remotecar.py
+++ remotecar.py
@@ -7,6 +7,35 @@ from pygame.locals import *
 #importing sleep 
 from time import sleep
 
+#json lib
+import json
+import cgi
+
+
+fs = cgi.FieldStorage()
+
+sys.stdout.write("Content-Type: application/json")
+
+sys.stdout.write("\n")
+sys.stdout.write("\n")
+
+
+result = {}
+result['success'] = True
+result['message'] = "The command Completed Successfully"
+result['keys'] = ",".join(fs.keys())
+
+d = {}
+for k in fs.keys():
+    d[k] = fs.getvalue(k)
+
+result['data'] = d
+
+sys.stdout.write(json.dumps(result,indent=1))
+sys.stdout.write("\n")
+
+sys.stdout.close()
+
 #init pygame
 pygame.init()
 #init joystick
